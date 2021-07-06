f319984caa84712ce69b49b570d38b19be7b1dc2
RachelRebecca <rachelrebecca67@gmail.com>
2021-07-05 22:26:31 -0400

    Initial commit

---
+++ app/src/main/java/com/example/thirteen_stones/activities/StatisticsActivity.java
@@ -0,0 +1,62 @@
+package com.example.thirteen_stones.activities;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.MenuItem;
+import android.view.View;
+
+
+import com.example.thirteen_stones.databinding.ActivityStatisticsBinding;
+
+import com.example.thirteen_stones.R;
+
+public class StatisticsActivity extends AppCompatActivity {
+
+    private ActivityStatisticsBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityStatisticsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setUpToolbar();
+
+        setUpFAB();
+    }
+
+    private void setUpToolbar() {
+        setSupportActionBar(binding.toolbar);
+        if (getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    private void setUpFAB() {
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home){
+            onBackPressed();
+            return true;
+        }
+        else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+}
